1143b5560fd91e71c1b944ad3dbcb1f83486f237
Renad-Elgabrouny <118405042+Renad-Elgabrouny@users.noreply.github.com>
2026-02-22 15:34:12 +0200

    [125] [Time Beats: 97.97%] [Memory Beats: 54.42%] - LeetPush

--- JS_PS/Valid-Palindrome.js
+++ JS_PS/Valid-Palindrome.js
@@ -3,28 +3,19 @@
 3 * @return {boolean}
 4 */
 5var isPalindrome = function(s) {
-6    let left = 0;
-7    let right = s.length - 1;
-8
-9    while (left < right) {
-10
-11        while (left < right && !isAlphaNum(s[left])) {
-12            left++;
-13        }
-14        while (left < right && !isAlphaNum(s[right])) {
-15            right--;
-16        }
-17        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-18            return false;
-19        }
+6  const cleaned = s
+7  .toLowerCase()
+8  .replace(/[^a-z0-9]/g, "");
+9  let n=cleaned.length
+10  let mid=Math.floor(n/2)
+11  for(let i=0;i<mid;i++)
+12  {
+13    if(cleaned[i]!==cleaned[n-i-1])
+14    {
+15       return false
+16    }
+17  }
+18  return true
+19
 20
-21        left++;
-22        right--;
-23    }
-24
-25    return true;
-26};
-27
-28function isAlphaNum(char) {
-29    return /[a-z0-9]/i.test(char);
-30}
+21};
